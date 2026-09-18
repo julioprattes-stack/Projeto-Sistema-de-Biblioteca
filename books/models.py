e701585eefd707d5a229699c666253b45192e4ab
Julio César <julioprattes@gmail.com>
2026-09-17 21:36:24 -0300

    criando models

--- books/models.py
+++ books/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class BookModel(models.Model):
+    title = models.CharField(max_length=50)
+    author = models.CharField(max_length=50)
+    category = models.CharField(max_length=50)
+    publication_yer = models.IntegerField()  #ano publicaçao
+    ibsn = models.CharField(max_length=50)
+    available = models.BooleanField()  #disponivel
+    created_at = models.DateTimeField(auto_now_add=True)  #criado_em
+
+    def __str__(self):
+        return f'{self.author} - {self.title}'
