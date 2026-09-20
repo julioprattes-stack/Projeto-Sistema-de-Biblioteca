7839f9d633ee251b4c62f8105ec9b005fafe4d31
Julio César <julioprattes@gmail.com>
2026-09-20 11:06:57 -0300

    Criando AuthorModels e tendo uma chave ForeingKey

--- books/admin.py
+++ books/admin.py
@@ -6,3 +6,8 @@ class BookAdmin(admin.ModelAdmin):
     list_display = ('author', 'title', 'publication_year', 'available',)
     search_fields = ('author', 'title')
     list_filter = ('available',)
+
+@admin.register(models.AuthorModels)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('name','nationality',)
+    search_fields = ('name',)
--- books/models.py
+++ books/models.py
@@ -1,8 +1,21 @@
 from django.db import models
 
+
+class AuthorModels(models.Model):
+    name = models.CharField(max_length=50)
+    nationality = models.CharField(max_length=50, blank=True)
+
+    def __str__(self):
+        return self.name
+
 class BookModel(models.Model):
     title = models.CharField(max_length=50)
-    author = models.CharField(max_length=50)
+    author = models.ForeignKey(
+        AuthorModels,
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True
+    )
     category = models.CharField(max_length=50, blank=True)
     publication_year = models.IntegerField()  #ano publicaçao
     ibsn = models.CharField(max_length=50, blank=True)
